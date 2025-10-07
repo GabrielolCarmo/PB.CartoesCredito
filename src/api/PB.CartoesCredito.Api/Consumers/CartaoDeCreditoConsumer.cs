4be77ecad204217b3b25e63812852e00fc3d88d5
Gabriel Carmo <gabriel_o.carmo@hotmail.com>
2025-10-07 10:41:38 -0300

    Api consumer da liberacao de credito

---
+++ src/api/PB.CartoesCredito.Api/Consumers/CartaoDeCreditoConsumer.cs
@@ -0,0 +1,30 @@
+﻿using MassTransit;
+using PB.CartoesCredito.Domain.Cartoes.Command;
+using PB.Commons.Api.Models;
+using PB.Commons.Infra.Kernel.Data;
+
+namespace PB.CartoesCredito.Api.Consumers
+{
+    public class CartaoDeCreditoConsumer(MediatR.IMediator mediator, IUnityOfWork uow) : IConsumer<CreditoDisponibilizadoMessage>
+    {
+        private readonly MediatR.IMediator _mediator = mediator;
+        private readonly IUnityOfWork _uow = uow;
+
+        public async Task Consume(ConsumeContext<CreditoDisponibilizadoMessage> context)
+        {
+            var message = context.Message;
+            var command = new EmitirCartoesDeCreditoCommand
+            {
+                ClientId = message.ClienteId,
+                Score = message.Score,
+                CreditoDisponivel = message.CreditoDisponivel
+            };
+
+            var result = await _mediator.Send(command);
+            if (result.IsSuccess)
+            {
+                await _uow.CommitTransactionAsync();
+            }
+        }
+    }
+}
---
+++ src/api/PB.CartoesCredito.Api/ServicesExtensions.cs
@@ -0,0 +1,45 @@
+﻿using MassTransit;
+using PB.CartoesCredito.Api.Consumers;
+using PB.CartoesCredito.Infra.Data;
+
+namespace PB.CartoesCredito.Api
+{
+    public static class ServicesExtensions
+    {
+        public static void AddApiServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDataServices();
+
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumer<CartaoDeCreditoConsumer>();
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(configuration["Rabbit:Host"] ?? "localhost", "/", h =>
+                    {
+                        h.Username(configuration["Rabbit:User"] ?? "guest");
+                        h.Password(configuration["Rabbit:Pass"] ?? "guest");
+                    });
+
+                    cfg.ReceiveEndpoint(configuration["RabbitMQ:PropostaQueue"] ?? throw new InvalidOperationException("RabbitMQ:ClientesQueue configuration is missing."), e =>
+                    {
+                        e.ConfigureConsumer<CartaoDeCreditoConsumer>(context);
+
+                        // caso todas as tentativas de reenvio imediato falhem, tenta reenviar a mensagem em 3 intervalos diferentes
+                        // com 5, 15 e 30 minutos de espera entre cada tentativa
+                        cfg.UseDelayedRedelivery(r => r.Intervals(TimeSpan.FromMinutes(5), TimeSpan.FromMinutes(15), TimeSpan.FromMinutes(30)));
+
+                        // caso de falha na entrega da mensagem, tenta reenviar 5 vezes imediatamente
+                        cfg.UseMessageRetry(r => r.Immediate(5));
+                    });
+
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Domain.ServicesExtensions).Assembly));
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Application.ServicesExtensions).Assembly));
+        }
+    }
+}
